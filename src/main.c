1906af926cb7a3e98402bab37a299240ded28b3f
Aotrix <aotrix56@gmail.com>
2022-03-15 23:41:27 +0100

    oui

--- src/main.c
+++ src/main.c
@@ -1,7 +1,12 @@
 #include "arbre_n.h"
 #include <stdio.h>
 
+void afficherMessage(char* message) {
+    printf(message);
+}
+
 int main(void) {
+    afficherMessage("Coucou !");
     
     return 0;
 }
